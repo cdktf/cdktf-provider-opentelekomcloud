dbbc6fe0ee4fe64eeba6483f9fe34fc36a9257c7
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-08-28 10:15:44 +0000

    chore(deps): Updated  by cdktf/cdktf-repository-manager@9fe1871

--- src/data-opentelekomcloud-css-flavor-v1/index.ts
+++ src/data-opentelekomcloud-css-flavor-v1/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/opentelekomcloud/opentelekomcloud/1.35.6/docs/data-sources/css_flavor_v1
 // generated from terraform resource schema
 
