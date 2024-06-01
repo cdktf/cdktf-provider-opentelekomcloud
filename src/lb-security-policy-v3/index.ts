062d167285447b67a9ee95f00b8fed3c4de476b8
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-06-01 03:49:25 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/lb-security-policy-v3/index.ts
+++ src/lb-security-policy-v3/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/opentelekomcloud/opentelekomcloud/1.36.10/docs/resources/lb_security_policy_v3
 // generated from terraform resource schema
 
