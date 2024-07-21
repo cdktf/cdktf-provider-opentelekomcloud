2d6bd9ae59e992724e0b9fe55c8fb30269badb4e
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-07-21 06:10:00 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@30ba874

--- src/data-opentelekomcloud-lb-member-ids-v2/index.ts
+++ src/data-opentelekomcloud-lb-member-ids-v2/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/opentelekomcloud/opentelekomcloud/1.36.14/docs/data-sources/lb_member_ids_v2
 // generated from terraform resource schema
 
