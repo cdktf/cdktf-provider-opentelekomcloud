95b7657d03842076373646cfd7d285e558dc3647
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-08-26 06:41:03 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/nat-snat-rule-v2/index.ts
+++ src/nat-snat-rule-v2/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/opentelekomcloud/opentelekomcloud/1.36.17/docs/resources/nat_snat_rule_v2
 // generated from terraform resource schema
 
