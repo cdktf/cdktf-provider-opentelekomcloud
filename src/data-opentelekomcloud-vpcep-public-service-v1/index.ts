579b59594c3a1bfae6e31ceab5b33ed53dd27c7f
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-09-22 06:14:13 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-opentelekomcloud-vpcep-public-service-v1/index.ts
+++ src/data-opentelekomcloud-vpcep-public-service-v1/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/opentelekomcloud/opentelekomcloud/1.36.19/docs/data-sources/vpcep_public_service_v1
 // generated from terraform resource schema
 
