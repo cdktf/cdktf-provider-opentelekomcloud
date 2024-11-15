9321c2c4eebc3b19b032e5f869b664337545dc68
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-11-15 03:53:31 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-opentelekomcloud-sdrs-domain-v1/index.ts
+++ src/data-opentelekomcloud-sdrs-domain-v1/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/opentelekomcloud/opentelekomcloud/1.36.25/docs/data-sources/sdrs_domain_v1
 // generated from terraform resource schema
 
