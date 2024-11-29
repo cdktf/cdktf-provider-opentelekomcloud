980fe6ece75b926535dbe47f2ca7dab3476fa7ba
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-11-29 03:54:20 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/vpcep-endpoint-v1/index.ts
+++ src/vpcep-endpoint-v1/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/opentelekomcloud/opentelekomcloud/1.36.26/docs/resources/vpcep_endpoint_v1
 // generated from terraform resource schema
 
