a70a647d20ee8c9f21d64f64d32c1e8dd9d0013d
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-12-08 06:15:37 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/dns-ptrrecord-v2/index.ts
+++ src/dns-ptrrecord-v2/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/opentelekomcloud/opentelekomcloud/1.36.26/docs/resources/dns_ptrrecord_v2
 // generated from terraform resource schema
 
