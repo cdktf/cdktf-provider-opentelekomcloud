cf6070917a415ec9bfdbb58052a7db21269efba9
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-12-19 15:00:36 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@7f995f5

--- src/data-opentelekomcloud-csbs-backup-policy-v1/index.ts
+++ src/data-opentelekomcloud-csbs-backup-policy-v1/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/opentelekomcloud/opentelekomcloud/1.36.27/docs/data-sources/csbs_backup_policy_v1
 // generated from terraform resource schema
 
