975de8b5b3f8d280805c9660248119b0f939e9ce
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-01-06 15:46:17 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/rms-policy-assignment-v1/index.ts
+++ src/rms-policy-assignment-v1/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/opentelekomcloud/opentelekomcloud/1.36.28/docs/resources/rms_policy_assignment_v1
 // generated from terraform resource schema
 
