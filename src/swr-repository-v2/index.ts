83e3f8a15527a42836934aa4926646674ce4ae7a
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-02-09 06:12:24 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/swr-repository-v2/index.ts
+++ src/swr-repository-v2/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/opentelekomcloud/opentelekomcloud/1.36.30/docs/resources/swr_repository_v2
 // generated from terraform resource schema
 
