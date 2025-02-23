2ea5fb21178a5fc73319c51c0d58248a46427164
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-02-23 06:10:36 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@6def3f6

--- src/data-opentelekomcloud-hss-hosts-v5/index.ts
+++ src/data-opentelekomcloud-hss-hosts-v5/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/opentelekomcloud/opentelekomcloud/1.36.31/docs/data-sources/hss_hosts_v5
 // generated from terraform resource schema
 
