537f304315308b56760aa6a1f275326d833960ba
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 12:15:50 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@4c005a5

--- src/data-opentelekomcloud-apigw-api-history-v2/index.ts
+++ src/data-opentelekomcloud-apigw-api-history-v2/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/opentelekomcloud/opentelekomcloud/1.36.39/docs/data-sources/apigw_api_history_v2
 // generated from terraform resource schema
 
