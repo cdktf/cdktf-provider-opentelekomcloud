3f67bc0b303568f4553b48bc159ab4278d24cf3e
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 14:53:08 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@4d35a18

--- src/data-opentelekomcloud-dcs-maintainwindow-v1/index.ts
+++ src/data-opentelekomcloud-dcs-maintainwindow-v1/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/opentelekomcloud/opentelekomcloud/1.36.39/docs/data-sources/dcs_maintainwindow_v1
 // generated from terraform resource schema
 
